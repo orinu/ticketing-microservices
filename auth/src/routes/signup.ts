f4d1ef1260f42d8f60a4b26476b3f7846f304a8b
Ori Nahum <orinah@rafael.co.il>
2022-08-07 17:25:05 +0300

    add validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,16 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signup", (req, res) => {
-  const { email, password } = req.body;
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    // error handler
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
 
-  if (!email || typeof email !== 'string') {
-      res.status(400).send('Provide a valid email')
+    const { email, password } = req.body;
+
+    console.log('Creating a user')
+  
+    res.send({})
+    
+
   }
-
-  // new User({ email, password })
-});
+);
 
 export { router as signupRouter };
-
