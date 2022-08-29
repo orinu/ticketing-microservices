8118550d3f3c4c39446794f1269c91245ede92ed
“Ori <ori.nahu@gmail.com>
2022-08-29 20:00:01 +0300

    add tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import { app } from "./app";
+
+const start = async () => {
+  // check if JWT_KEY defined
+  if (!process.env.JWT_KEY) {
+    throw new Error("process.env.JWT_KEY not found");
+  }
+  // connect to db
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000 ");
+  });
+};
+
+start();
