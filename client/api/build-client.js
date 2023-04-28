e6d4004a84a6fb75a5185e54f2ec0e52557090b8
“Ori <ori.nahu@gmail.com>
2023-04-28 21:31:23 +0300

    change deploy ingress yaml

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,10 +4,17 @@ export default ({ req }) => {
      // on server - request made to http://ingress-nginx...
   if (typeof window === "undefined") {
     return axios.create({
-        baseURL: 'www.uniqueautohub.store',
+        baseURL: 'http://www.uniqueautohub.store/',
         headers: req.headers
     })
   }
+  // if (typeof window === "undefined") {
+  //   return axios.create({
+  //       baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+  //       headers: req.headers
+  //   })
+  // }
+  
   // on browser, no need to change base url
   else {
     return axios.create({
